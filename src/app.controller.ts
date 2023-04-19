5327ac525c267cfc3ca03b6c9628f43cf229fbd0
Faridho Luedfi <faridho.lutfi@gmail.com>
2023-04-19 22:53:08 +0700

    init redis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, Inject, UseInterceptors } from '@nestjs/common';
-import { CACHE_MANAGER, CacheInterceptor } from '@nestjs/cache-manager';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Cache } from 'cache-manager';
 
 @Controller()
 export class AppController {
@@ -11,6 +9,6 @@ export class AppController {
 
   @Get()
   async getHello() {
-    return "ok"
+    return this.appService.getHello()
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,26 @@ import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+import { async } from 'rxjs';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        isGlobal: true,
+        store: redisStore,
+        host: config.get("REDIS_HOST"),
+        port: config.get("REDIS_PORT"),
+        ttl: 5, // second
+      })
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
